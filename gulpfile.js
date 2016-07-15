db229df278445c27b6c849b66bbc4a483514bc15
hbaughman <hbaughman741@gmail.com>
2016-07-15 14:19:36 -0400

    Transpile ES6

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp')
 var sass = require('gulp-sass')
+var babel = require('gulp-babel')
 
 gulp.task('sass', function() {
   return gulp.src('src/sass/style.+(sass|scss)')
@@ -11,4 +12,15 @@ gulp.task('sass:watch', function() {
   gulp.watch('src/sass/**/*.+(sass|scss)', ['sass'])
 })
 
-gulp.task('default', ['sass', 'sass:watch'])
+
+gulp.task('babelify', function() {
+  return (gulp.src('src/js/index.js'))
+    .pipe(babel())
+    .pipe(gulp.dest('dist/assets/js/'))
+})
+
+gulp.task('babelify:watch', function() {
+  gulp.watch('src/js/**/*.+(js|json)', ['babelify'])
+})
+
+gulp.task('default', ['sass', 'sass:watch', 'babelify', 'babelify:watch'])
